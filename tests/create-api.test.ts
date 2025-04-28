d4782405fa304c3218a4564615b42faeca68afef
akbbhatt <akbbhatt@gmail.com>
2025-04-28 23:07:33 +0200

    added create and update users tests

---
+++ tests/create-api.test.ts
@@ -0,0 +1,45 @@
+import test, { expect } from '@playwright/test';
+import { validateSchema } from '../src/schemaValidator';
+import userSchema from './schema/create-user.schema.json';
+
+const headers: Record<string, string> = {
+    'Content-Type': 'application/json',
+};
+
+test('Create API - Validate /posts endpoint', async ({ request }) => {
+    const response = await request.post('/posts', {
+        data: {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        },
+        headers: headers,
+    });
+    const data = await response.json();
+    const status = response.status();
+    console.log('response:', data);
+    console.log('status:', status);
+    const valid = validateSchema(userSchema, data);
+    if (!valid.validate) {
+        console.error('Schema validation errors:', valid.errors);
+    }
+    expect(valid.validate).toBeTruthy();
+    expect(status).toBe(201);
+});
+
+test('Create and get the newly created user', async ({ request }) => {
+    const response = await request.post('/posts', {
+        data: { title: 'foo', body: 'bar', userId: 1 },
+        headers: headers,
+    });
+
+    const userId = (await response.json()).id;
+    const getUserResponse = await request.get(`/posts/${userId}`, { headers: headers });
+    //TODO: Since this is a fake API, the following lines will not work as expected
+    expect(getUserResponse.status()).toBe(200);
+    const getUserData = await getUserResponse.json();
+    expect(getUserData.id).toEqual(userId);
+});
+
+//TODO: Implement the following test after delete API is available
+test('Create and delete a user', async ({ request }) => {});
