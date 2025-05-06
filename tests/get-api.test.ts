f13dfb8942f1701ace46d894eea3af230e89ef99
akbbhatt <akbbhatt@gmail.com>
2025-05-06 22:53:17 +0200

    More assertions in get api tests

--- tests/get-api.test.ts
+++ tests/get-api.test.ts
@@ -14,12 +14,22 @@ test.describe('API Tests', () => {
         expect(response.status()).toBe(200);
         expect(Array.isArray(data)).toBeTruthy();
         expect(data.length).toBeGreaterThan(0);
+        expect(data).toContainEqual({
+            userId: 1,
 
+            title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+            id: 1,
+            body:
+                'quia et suscipit\n' +
+                'suscipit recusandae consequuntur expedita et cum\n' +
+                'reprehenderit molestiae ut ut quas totam\n' +
+                'nostrum rerum est autem sunt rem eveniet architecto',
+        });
         const schemaValidator = validateSchema(userSchema, data);
-        expect(schemaValidator.validate).toBeTruthy();
         if (!schemaValidator.validate) {
             console.error('Schema validation errors:', schemaValidator.errors);
         }
+        expect(schemaValidator.validate).toBeTruthy();
     });
 
     test('Get a specific user', async ({ request }) => {
@@ -27,5 +37,11 @@ test.describe('API Tests', () => {
         const data = await response.json();
         expect(response.status()).toBe(200);
         console.log('Response data:', data);
+        // Partial match by not including body field in the expected object
+        expect(data).toMatchObject({
+            userId: 1,
+            id: 1,
+            title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+        });
     });
 });
